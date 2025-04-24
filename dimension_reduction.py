6d5a57dde7aa61ca06cd66fc5e0fc7139208c12b
Haim Lavi <HaimL1@verifone.com>
2025-04-24 13:01:43 +0300

    tsne 54

--- dimension_reduction.py
+++ dimension_reduction.py
@@ -38,6 +38,102 @@ dimension_reduction_methods = [
 ]
 
 
+def calculate_statistics_for_clusters(df, entry, list_stats: list, list_stats_test: list):
+    cluster_labels = entry['opt_labels']
+    num_clusters = entry['opt_k']
+    reducer_display_name = entry['reducer_display_name']
+    clustering_display_name = entry['clustering_display_name']
+
+    if num_clusters > 0:
+        class_folder = os.path.join('output', 'classification')
+
+        if not os.path.exists(class_folder):
+            os.makedirs(class_folder)
+
+        file_name = f'{reducer_display_name}-{clustering_display_name}'
+
+        file_name_heat = f'{file_name}-heatmap.png'
+
+        class_file_heat = os.path.join(class_folder, file_name_heat)
+
+        df['cluster'] = cluster_labels
+
+        cluster_summary = df.groupby('cluster').mean().round(2)
+        # print(cluster_summary)
+
+        plt.figure(figsize=(12, 6))
+        sns.heatmap(cluster_summary, annot=True, cmap='coolwarm')
+        plt.title("Cluster Feature Means")
+        plt.savefig(class_file_heat, dpi=300)
+
+        df.drop(columns=['cluster'])
+
+        file_name_csv = f'{file_name}.txt'
+
+        class_file = os.path.join(class_folder, file_name_csv)
+
+        X = df
+        y = cluster_labels
+
+        clf = RandomForestClassifier(random_state=42)
+        clf.fit(X, y)
+
+        importances = clf.feature_importances_
+        feature_names = X.columns
+
+        importance_df = pd.Series(importances, index=feature_names).sort_values(ascending=False)
+
+        importance_df.to_csv(class_file)
+
+        file_name_image = f'{file_name}.png'
+
+        class_image_file = os.path.join(class_folder, file_name_image)
+
+        plt.figure(figsize=(10, 6))
+        importance_df.plot(kind='barh', color='teal')
+        plt.title("Feature Importance by Random Forest")
+        plt.xlabel("Importance")
+        plt.tight_layout()
+        plt.savefig(class_image_file, dpi=300)
+
+        for col in df.columns:
+            arr = df.get(col).tolist()
+
+            list_clusters = [[] for i in range(num_clusters)]
+
+            for i in range(len(arr)):
+                label = cluster_labels[i]
+
+                if label < len(list_clusters):
+                    cluster = list_clusters[label]
+                    cluster.append(arr[i])
+
+            f_stat, p_anova = f_oneway(*list_clusters)
+
+            h_stat = None
+            p_kruskal = None
+
+            try:
+                h_stat, p_kruskal = kruskal(*list_clusters)
+            except Exception as e:
+                _ = e
+
+            if p_anova < p_value_threshold and p_kruskal < p_value_threshold:
+                df0 = pd.DataFrame({
+                    'values': arr,
+                    'groups': cluster_labels
+                })
+
+                dunn_result = sp.posthoc_dunn(df0, val_col='values', group_col='groups', p_adjust='bonferroni')
+
+                list_stats_test.append((reducer_display_name, clustering_display_name, col, p_anova, p_kruskal))
+
+            list_stats.append(
+                (reducer_display_name, clustering_display_name, col, f_stat, p_anova, h_stat, p_kruskal))
+
+    return list_stats, list_stats_test
+
+
 def calculate(csv_file, target_column=None, drop_target_column: bool = True, columns_to_drop: list = None,
               csv_sep=',', k_min=2, k_max=22):
     # Load data
@@ -114,97 +210,8 @@ def calculate(csv_file, target_column=None, drop_target_column: bool = True, col
     list_stats_test: list = []
 
     for entry in opt_cluster_scores:
-        cluster_labels = entry['opt_labels']
-        num_clusters = entry['opt_k']
-        reducer_display_name = entry['reducer_display_name']
-        clustering_display_name = entry['clustering_display_name']
-
-        if num_clusters > 0:
-            class_folder = os.path.join('output', 'classification')
-
-            if not os.path.exists(class_folder):
-                os.makedirs(class_folder)
-
-            file_name = f'{reducer_display_name}-{clustering_display_name}'
-
-            file_name_heat = f'{file_name}-heatmap.png'
-
-            class_file_heat = os.path.join(class_folder, file_name_heat)
-
-            df['cluster'] = cluster_labels
-
-            cluster_summary = df.groupby('cluster').mean().round(2)
-            # print(cluster_summary)
-
-            plt.figure(figsize=(12, 6))
-            sns.heatmap(cluster_summary, annot=True, cmap='coolwarm')
-            plt.title("Cluster Feature Means")
-            plt.savefig(class_file_heat, dpi=300)
-
-            df.drop(columns=['cluster'])
-
-            file_name_csv = f'{file_name}.txt'
-
-            class_file = os.path.join(class_folder, file_name_csv)
-
-            X = df
-            y = cluster_labels
-
-            clf = RandomForestClassifier(random_state=42)
-            clf.fit(X, y)
-
-            importances = clf.feature_importances_
-            feature_names = X.columns
-
-            importance_df = pd.Series(importances, index=feature_names).sort_values(ascending=False)
-
-            importance_df.to_csv(class_file)
-
-            file_name_image = f'{file_name}.png'
-
-            class_image_file = os.path.join(class_folder, file_name_image)
-
-            plt.figure(figsize=(10, 6))
-            importance_df.plot(kind='barh', color='teal')
-            plt.title("Feature Importance by Random Forest")
-            plt.xlabel("Importance")
-            plt.tight_layout()
-            plt.savefig(class_image_file, dpi=300)
-
-            for col in df.columns:
-                arr = df.get(col).tolist()
-
-                list_clusters = [[] for i in range(num_clusters)]
-
-                for i in range(len(arr)):
-                    label = cluster_labels[i]
-
-                    if label < len(list_clusters):
-                        cluster = list_clusters[label]
-                        cluster.append(arr[i])
-
-                f_stat, p_anova = f_oneway(*list_clusters)
-
-                h_stat = None
-                p_kruskal = None
-
-                try:
-                    h_stat, p_kruskal = kruskal(*list_clusters)
-                except Exception as e:
-                    _ = e
-
-                if p_anova < p_value_threshold and p_kruskal < p_value_threshold:
-                    df0 = pd.DataFrame({
-                        'values': arr,
-                        'groups': cluster_labels
-                    })
-
-                    dunn_result = sp.posthoc_dunn(df0, val_col='values', group_col='groups', p_adjust='bonferroni')
-
-                    list_stats_test.append((reducer_display_name, clustering_display_name, col, p_anova, p_kruskal))
-
-                list_stats.append(
-                    (reducer_display_name, clustering_display_name, col, f_stat, p_anova, h_stat, p_kruskal))
+        list_stats, list_stats_test = calculate_statistics_for_clusters(df, entry, list_stats,
+                                                                        list_stats_test)
 
     clusters_file_path = os.path.join('output', 'clusters.txt')
 
@@ -290,8 +297,12 @@ def calculate_dimension_reduction(df_scaled, reducer_index, labels, target_colum
 
             new_highest_score = new_opt_score['highest_score']
 
-            if new_highest_score > existing_highest_score:
-                most_optimal_cluster = new_opt_score
+            if new_highest_score is not None:
+                if existing_highest_score is None:
+                    existing_highest_score = new_highest_score
+
+                if new_highest_score > existing_highest_score:
+                    most_optimal_cluster = new_opt_score
 
         save_results_to_image(reducer_display_name, cluster_display_name, num_comps, labels, results, clusters)
 
@@ -374,7 +385,7 @@ arr_files: list = [
     (r'ds\alzheimers_disease_data.csv', ['PatientID']),
     (r'ds\health_data.csv', ['id']),
     (r'ds\UserCarData.csv', ['Sales_ID'], ',', (2, 40), 'sold'),
-    (r'ds\schizophrenia_dataset.csv', ['Patient_ID'], ',', (2, 22), 'Diagnosis')
+    (r'ds\schizophrenia_dataset.csv', ['Patient_ID'], ',', (2, 5), 'Diagnosis')
 ]
 
 file_tuple: tuple = arr_files[-1]
